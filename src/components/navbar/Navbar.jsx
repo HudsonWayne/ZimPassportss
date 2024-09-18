6774ea2fcdcef4d36631cec37e4ea70780da5bbb
HudsonWayne <waynewaynebenhura@gmail.com>
2024-09-18 10:26:20 +0200

    added margin left and right

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,8 @@ import logo from "../../assets/images/logo.svg";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGlobe } from '@fortawesome/free-solid-svg-icons';
-// import { faApple } from '@fortawesome/free-brands-svg-icons'; // Corrected import
+import { faApple } from '@fortawesome/free-brands-svg-icons';
+
 
 const Navbar = () => {
     return (
@@ -16,7 +17,7 @@ const Navbar = () => {
                     <li><Link to="/"> <FontAwesomeIcon icon={faGlobe} size="2x" /></Link></li>
                     <div className="buttons">
                         <button type="button">Upload Photo</button>
-                        {/* <button type="button"><FontAwesomeIcon icon={faApple} size="2x" /> Download</button> */}
+                        <button type="button"><FontAwesomeIcon icon={faApple} size="2x" /> Download</button>
                     </div>
                 </ul>
             </nav>
